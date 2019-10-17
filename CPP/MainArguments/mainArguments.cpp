43a322ada1b00be47630ad779c43295a6836a71e
Alain Dekker <alain.dekker@teraki.com>
2019-10-17 17:18:01 +0200

    Added test comments

--- CPP/MainArguments/mainArguments.cpp
+++ CPP/MainArguments/mainArguments.cpp
@@ -6,5 +6,6 @@ int main(int argc, char **argv)
     // This application shows an example of how to process arguments to "main"
     std::cout << "Test arguments to main function\n\n";
     std::cout << "Number of arguments: " << argc << std::endl;
+    // test comment
     return 0;
 }
--- CPP/MainArguments/mainArguments.hpp
+++ CPP/MainArguments/mainArguments.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
 void ShowHelp();
-//test
+//test comment as well
