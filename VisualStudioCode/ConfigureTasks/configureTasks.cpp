0b0743560d2961bb88978c331e0a7c4003b3230a
Alain Dekker <alain.dekker@teraki.com>
2019-10-17 14:19:27 +0200

    Renaming files and adding .vscode folder

---
+++ VisualStudioCode/ConfigureTasks/configureTasks.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "configureTasks.hpp"
+
+int main(int argc, char **argv)
+{
+    // This tutorial is about configuring basic tasks in Visual Studio Code
+    std::cout << "Hello from ConfigureTasks" << std::endl;
+    return 0;
+}
