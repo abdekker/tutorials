0b860d7c8301e08cffa4001203b2a0ab88a35829
Alain Dekker <alain.dekker@teraki.com>
2019-10-17 15:45:03 +0200

    -m Added a simple launch configuration

--- VisualStudioCode/ConfigureTasks/configureTasks.cpp
+++ VisualStudioCode/ConfigureTasks/configureTasks.cpp
@@ -5,5 +5,6 @@ int main(int argc, char **argv)
 {
     // This tutorial is about configuring basic tasks in Visual Studio Code
     std::cout << "Hello from ConfigureTasks" << std::endl;
+    std::cout << "Number of arguments: " << argc << std::endl;
     return 0;
 }
