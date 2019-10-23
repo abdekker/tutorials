f88acf9c24d99a70e606f1acc9761478b9e8c337
Alain Dekker <alain.dekker@teraki.com>
2019-10-23 16:08:10 +0200

    Added environment variable demonstration

--- CommandLine/Bash-Linux/Parameters.sh
+++ CommandLine/Bash-Linux/Parameters.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Demonstrates checking parameters passed to the script
 # Example: bash ./Parameters.sh --help --unknown
-version="1.0.0"
+version="1.0.1"
 
 show_help()
 {
@@ -19,42 +19,64 @@ show_version()
 }
 
 interactive=TRUE
-declare -i paramcount=0     # Declare as integer, but simply "paramcount=0" also works
+echo The \"\$\#\" environment variable is the number of parameters to this script
+echo "  "Raw count=$#
+if [ $# -gt 0 ]; then
+    echo "  "First parameter is \"$1\"
+fi
+echo
+
+echo Processing known parameters
+declare -i paramcount=0     # Declare as integer, but "paramcount=0" also works
 declare -i paramknown=0
+declare -i IsHelp=0
+declare -i IsVersion=0
 for param in "$@"; do
     # Alternatives for incrementing an integer include:
     # ((var=var+1))
     # let "var=var+1"
     ((paramcount+=1))
 
-    # "Help" checked one way...
+    # Help checked one way...
     if echo $param | grep "^--help" > /dev/null 2> /dev/null; then
-        show_help
+        ((IsHelp+=1))
         ((paramknown+=1))
     fi
 
-    # "Version" checked another way...
+    # Version checked another way...
     if [ $param = "--version" ]; then
-        show_version
+        ((IsVersion+=1))
         ((paramknown+=1))
     fi
 done
 
+# Display the number of known parameters
 if [ $paramcount = 0 ]; then
-    echo No parameters supplied...
+    echo "  "No parameters supplied...
 else
-    # Add a blank line if any parameters were processed; "-gt" is "greater than"
-    if [ $paramknown -gt 0 ]; then
-        echo
-    fi
-
     # Total number of parameters
-    echo Total parameters: $paramcount
+    echo "  "Total parameters: $paramcount
 
     # Known parameters (the "elif [...]" can simply be "else", but demonstrates "else if")
     if [ $paramknown = 0 ]; then
-        echo Known parameters: None
+        echo "  "Known parameters: None
     elif [ $paramknown -gt 0 ]; then
-        echo Known parameters: $paramknown
+        echo "  "Known parameters: $paramknown
+    fi
+fi
+
+# Now use the parameters (if any)
+if [ $IsHelp -gt 0 ] || [ $IsVersion -gt 0 ]; then
+    # Show help. The following are all equivalent:
+        # if [ $IsHelp -gt 0 ]; then
+        # if [ ${IsHelp} -gt 0 ]; then
+        # if [ "$IsHelp" -gt 0 ]; then
+    if [ $IsHelp -gt 0 ]; then
+        echo; show_help
+    fi
+
+    if [ $IsVersion -gt 0 ]; then
+        # Show version
+        echo; show_version
     fi
 fi
