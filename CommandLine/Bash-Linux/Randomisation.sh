2e8c9d38f57ec09d957ca3eba062f500e9a647f3
Alain Dekker <alain.dekker@teraki.com>
2019-10-30 15:54:16 +0100

    Additional randomisation options

--- CommandLine/Bash-Linux/Randomisation.sh
+++ CommandLine/Bash-Linux/Randomisation.sh
@@ -3,34 +3,50 @@
 # Example: bash ./Randomisation.sh
 # Adapted from: https://unix.stackexchange.com/questions/230673
 
-# Constants (can also be defined with "readonly VAR=VALUE")
+# Constants can also be defined with "readonly VAR=VALUE"
 declare -i DEFAULT_PWD_LENGTH=10
 
 # Functions
-funcRandomPWGEN()   # Can also be "function NAME()"
+funcPwgenSecure()   # Can also be "function NAME()"
 {
   # Generate a random string using the "pwgen" password generator
-  # From the command-line
 
-  # Set password length
+  # 1st parameter: Password length
   pwgenLength=$1
   if [ -z "$1" ]; then
     pwgenLength=$DEFAULT_PWD_LENGTH
   fi
 
-  # Exclude special characters? Leave blank to include all special characters.
+  # 2nd parameter: Characters to exclude. Leave blank to include all characters.
+  # Note: Typical use is to exclude special characters that might, for example, prevent
+  # the generated string being used to create a file or folder on Linux.
   if [ -n "$2" ]; then
-    RAND_PWGEN=$(pwgen -sy $pwgenLength -r {$"$2"})
+    FUNC_RETURN=$(pwgen -sy -r {$"$2"} $pwgenLength 1)
   else
-    RAND_PWGEN=$(pwgen -sy $pwgenLength)
+    FUNC_RETURN=$(pwgen -sy $pwgenLength 1)
   fi
+}
+
+funcRandomGPW()
+{
+  # Generate a random string using "gpw" (simpler, though less secure, than "pwgen")
+
+  # 1st parameter: Password length
+  pwgenLength=$1
+  if [ -z "$1" ]; then
+    pwgenLength=$DEFAULT_PWD_LENGTH
+  fi
+
+  # To generate multiple strings, change the 1st parameter to "gpw" (eg. 5)
+  FUNC_RETURN=$(gpw 1 $pwgenLength)
 }
 
 interactive=TRUE
 echo "### Randomisation in bash scripts ###"
+echo Note: String examples are $DEFAULT_PWD_LENGTH characters long
 echo
 
-echo Using \"pwgen\" \(secure password generator\)
+echo Using \"pwgen\" \(password generator, secure usage with \"-y\"\)
 # Check whether pwgen is installed...
 pwgenPath=$(which pwgen)
 if [ -z $pwgenPath ]; then
@@ -41,20 +57,51 @@ fi
 
 # Is the environment variable $PWGEN_SPECIAL declared?
 declare -i isStdExcludeSpecial=1
-if [ -z $PWGEN_SPECIAL1 ]; then
+if [ -z $PWGEN_SPECIAL ]; then
   echo "  "Missing environment variable \"PWGEN_SPECIAL\" in .bashrc
   echo "  "Recommend \"export PWGEN_SPECIAL=\'\"\@\?\^\&\*\(\)\`\:\~\?\;\:\[\]\{\}\.\,\\\/\|\"
   PWGEN_SPECIAL_TMP=\'\"\@\?\^\&\*\(\)\`\:\~\?\;\:\[\]\{\}\.\,\\\/\|
   isStdExcludeSpecial=0
 fi
 
-# Note: To include all special characters, use "funcRandomPWGEN LENGTH".
-# Note: In general, this cannot be used to create a folder in Linux.
+# Note: To include all special characters, use "funcPwgenSecure LENGTH" (no 2nd parameter).
 if [ $isStdExcludeSpecial -gt 0 ]; then
-  funcRandomPWGEN $DEFAULT_PWD_LENGTH $PWGEN_SPECIAL
+  funcPwgenSecure $DEFAULT_PWD_LENGTH $PWGEN_SPECIAL
 else
-  funcRandomPWGEN $DEFAULT_PWD_LENGTH $PWGEN_SPECIAL_TMP
+  funcPwgenSecure $DEFAULT_PWD_LENGTH $PWGEN_SPECIAL_TMP
 fi
-echo Generated $DEFAULT_PWD_LENGTH-char string = $RAND_PWGEN \(Could use eg. to create a file or folder with \"touch\" or \"mkdir\"\)
-# Note: Since special characters have been excluded, this can be used to generate a file or folder in Linux
+RAND_PWGEN=$FUNC_RETURN
+echo "  "$DEFAULT_PWD_LENGTH-char string \(Limit specials"   "\)  = $RAND_PWGEN \(Could use eg. to create a file or folder with \"touch\" or \"mkdir\"\)
+
+funcPwgenSecure $DEFAULT_PWD_LENGTH
+RAND_PWGEN=$FUNC_RETURN
+echo "  "$DEFAULT_PWD_LENGTH-char string \(All special chars\) = $RAND_PWGEN \(User password, no guarantee to successfully create file or folder\)
+# Note: If special characters are excluded, this can be used to generate a file or folder in Linux
+echo .
+
+echo Using \"pwgen\" \(additional examples, less secure usage\)
+RAND_PWGEN=$(pwgen $DEFAULT_PWD_LENGTH 1)
+echo "  "pwgen $DEFAULT_PWD_LENGTH 1"      " = $RAND_PWGEN \(upper, lower, numbers\)
+RAND_PWGEN=$(pwgen -c $DEFAULT_PWD_LENGTH 1)
+echo "  "pwgen -c $DEFAULT_PWD_LENGTH 1"   " = $RAND_PWGEN \(at least one upper, lower, numbers\)
+RAND_PWGEN=$(pwgen -A $DEFAULT_PWD_LENGTH 1)
+echo "  "pwgen -A $DEFAULT_PWD_LENGTH 1"   " = $RAND_PWGEN \(no upper, lower, numbers\)
+RAND_PWGEN=$(pwgen -n $DEFAULT_PWD_LENGTH 1)
+echo "  "pwgen -n $DEFAULT_PWD_LENGTH 1"   " = $RAND_PWGEN \(upper, lower, at least one number\)
+RAND_PWGEN=$(pwgen -0 $DEFAULT_PWD_LENGTH 1)
+echo "  "pwgen -0 $DEFAULT_PWD_LENGTH 1"   " = $RAND_PWGEN \(upper, lower, no numbers\)
+echo .
+
+echo Using \"gpw\" \(simple pronounceable password generator\)
+# Check whether gpw is installed...
+gpwPath=$(which gpw)
+if [ -z $gpwPath ]; then
+  echo "  "Command \'gpw\' not found, but can be installed with:
+  echo "  "sudo apt install gpw
+  exit
+fi
+
+funcRandomGPW $DEFAULT_PWD_LENGTH
+RAND_GPW=$FUNC_RETURN
+echo $DEFAULT_PWD_LENGTH-char string = $RAND_GPW \(Lowercase letters only - can be used to create a file or folder\)
 echo .
