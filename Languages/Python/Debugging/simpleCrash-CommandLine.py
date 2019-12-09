fde6c619c7dfe67f7309344a8f5d35ecb188b46e
Alain Dekker <alain.dekker@teraki.com>
2019-12-09 09:44:51 +0100

    Small updates to the comments

--- Languages/Python/Debugging/simpleCrash-CommandLine.py
+++ Languages/Python/Debugging/simpleCrash-CommandLine.py
@@ -1,8 +1,11 @@
 # Refer to Notes-Python-Debugging.txt for a more detailed description
 
 # Step 1: Run from Terminal with "python FILE"
-# Step 2: Uncomment "import pdb; pdb.set_trace()" and repeat. Step lines with "n". Type "x" to see the its' current value.
-# Step 3: Uncomment "x = x / 0" and repeat. You get an exception on this line.
+# Step 2: Uncomment "import pdb; pdb.set_trace()" and run again
+# Step 3: When the breakpoint is hit, execution will stop
+#   Step lines with "n"
+#   Type "x" to see the current value of this variable
+# Step 4: Uncomment "x = x / 0" and repeat. You get an exception on this line.
 
 print("hello")
 x = 5
--- Languages/Python/Debugging/simpleCrash-VSCode.py
+++ Languages/Python/Debugging/simpleCrash-VSCode.py
@@ -1,7 +1,8 @@
 # Refer to Notes-Python-Debugging.txt for a more detailed description
 
-# Step 1: Set breakpoint on the first line "print("hello")". Debug and see output in Terminal window.
-# Step 2: Uncomment "x = x / 0" and repeat. You get an exception on this line.
+# Step 1: Set breakpoint on the first line "print("hello")"
+# Step 2: Debug and see output in Terminal window
+# Step 3: Uncomment "x = x / 0" and repeat. You get an exception on this line.
 
 print("hello")
 x = 5
