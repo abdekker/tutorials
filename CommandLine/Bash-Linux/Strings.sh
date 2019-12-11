12cdf2f53482feeb438c8dec58dd9ebd701f6867
Alain Dekker <alain.dekker@teraki.com>
2019-12-11 14:48:29 +0100

    Working with an array of strings

--- CommandLine/Bash-Linux/Strings.sh
+++ CommandLine/Bash-Linux/Strings.sh
@@ -5,7 +5,7 @@
 
 interactive=TRUE
 echo "### Strings and string manipulation in bash scripts ###"
-STR1="Hello World! 123"
+STR1="Hello World!"
 STR2=orl
 STR3=$STR1","$STR2  # Concatenatation with comma separator; use "STR3=$STR1$STR2" for no comma
 echo STR1 = $STR1
@@ -26,6 +26,26 @@ echo "  "${STR1:2:5}                    # llo W
 echo Using \"expr substr\" \(1-based\) eg. \$\(expr substr \"\$STR\" 1 1\)
 echo "  "$(expr substr "$STR1" 1 1)     # H
 echo "  "$(expr substr "$STR1" 3 5)     # llo W
+echo .
+
+echo "# Array of strings #"
+declare -a myStrings=( "string1" "num2" "last" )
+declare -i numStrings=${#myStrings[@]}
+echo "  "Our array has $numStrings elements
+if [ $numStrings -gt 0 ]; then
+    listOfStrings=""
+    declare -i stringPos=0
+    for str in "${myStrings[@]}"; do
+        ((stringPos++))
+        listOfStrings+=$str
+        if [ $stringPos -lt $numStrings ]; then
+            listOfStrings+=", "
+        fi
+    done
+
+    # Display the list of tags to the user
+    echo "  "List of strings: $listOfStrings
+fi
 echo .
 
 echo All done!
