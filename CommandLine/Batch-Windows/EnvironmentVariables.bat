4bee23d2b004110020a067719c469d1af98a3b38
Alain Dekker <abdekker.online@gmail.com>
2020-01-26 16:01:51 +0100

    Minor change in comment

--- CommandLine/Batch-Windows/EnvironmentVariables.bat
+++ CommandLine/Batch-Windows/EnvironmentVariables.bat
@@ -3,13 +3,19 @@
 rem This script covers some aspects of environment variables in DOS and Windows
 
 echo ### Use an environment variable to prevent a script running multiple times ###
-rem This trick prevents circular calls, and similar variations, such as:
+rem This trick can be used to prevent circular calls, and variations, such as:
 rem ScriptA) call ScriptB
 rem ScriptB) call ScriptA
 if defined SCRIPT_IS_RUNNING (exit)
-set SCRIPT_IS_RUNNING=1
 echo Now "SCRIPT_IS_RUNNING" is defined
 echo .
+
+echo ### Incrementing environment variables ###
+set TEST_INTEGER=1
+set /A TEST_INTEGER=TEST_INTEGER+1
+if %TEST_INTEGER%==2 (echo   TEST_INTEGER is 2) else (echo   TEST_INTEGER is something else)
+echo .
+pause
 
 echo ### Check whether an environment variable is defined ###
 echo 1) Using "defined"
@@ -19,7 +25,8 @@ if defined TEST_PARAM_1 (echo   Parameter 1 is defined) else (echo   Parameter 1
 if defined TEST_PARAM_2 (echo   Parameter 2 is defined) else (echo   Parameter 2 is NOT defined!)
 echo .
 
-echo 2a) Using "==" (using qutoation marks)
+echo 2a) Using "==" (using quotation marks)
+rem Note: Quotation marks are useful if the variable may have spaces
 if "%TEST_PARAM_1%"=="" (echo   Parameter 1 is NOT defined) else (echo   Parameter 1 is defined!)
 if "%TEST_PARAM_2%"=="" (echo   Parameter 2 is NOT defined) else (echo   Parameter 2 is defined!)
 echo .
