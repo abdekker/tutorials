0618e8b11436e2b56aaaee22993e634a668fd7a4
Alain Dekker <abdekker.online@gmail.com>
2020-03-31 10:50:35 +0200

    Added example with std::stringStream

--- Languages/CPP/Strings/StringsInCPP/StringsInCPP.cpp
+++ Languages/CPP/Strings/StringsInCPP/StringsInCPP.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <sstream>		// For std::stringstream
 using namespace std;
 
 void ConstructStdString()
 {
 	// Various ways to construct a std::string
+	// Note: Some of these examples may generate reports (including false positives) of memory
+	// leaks in tool such as valgrind. These may be related to how the memory is managed internally
+	// with the standard library.
 	cout << "Construct a std::string\n";
 
 	{
@@ -12,48 +16,63 @@ void ConstructStdString()
 	}
 
 	{
-		string s("Hello std::string 2");
+		string s1 = "Hello std::string 2";
+		string s2 = s1;
+		cout << "  By simple assignment\t\t\t" << s2 << "\n";
+	}
+
+	{
+		string s("Hello std::string 3");
 		cout << "  From string literal\t\t\t" << s << "\n";
 	}
 
 	{
-		const char* c = "Hello std::string 3a";
+		const char* c = "Hello std::string 4a";
 		string s(c);
 		cout << "  From char*\t\t\t\t" << s << "\n";
 	}
 
 	{
-		const char* c = "Hello std::string 3b";
+		const char* c = "Hello std::string 4b";
 		size_t len = strlen(c);
 		string s(c, len);		// Use "len-2" (eg) to remove the last two characters
 		cout << "  From char*\t\t\t\t" << s << "\n";
 	}
 
 	{
-		const char* c = "Hello std::string 3c";
+		const char* c = "Hello std::string 4c";
 		string s = c;	// Direct assignment
 		cout << "  From char*\t\t\t\t" << s << "\n";
 	}
 
 	{
 		string s;
-		const char* c = "Hello std::string 4";
+		const char* c = "Hello std::string 5";
 		size_t len = strlen(c);
 		s.assign(c, len);	// Only if the string already exists
 		cout << "  Using std::string::assign\t\t" << s << "\n";
 	}
 
 	{
+		const char* c = "Hello std::string 6";
+		std::string s;
+		s.append(c);
+		cout << "  Using std::string::append\t\t" << s << "\n";
+	}
+
+	{
 		const char* c1 = "Hello ";
-		const char* c2 = "std::string 5"; 
+		const char* c2 = "std::string 7"; 
 		string s = string(c1) + string(c2);
 		cout << "  Using std::string::operator+\t\t" << s << "\n";
 	}
 
 	{
-		string s1 = "Hello std::string 6";
-		string s2 = s1;
-		cout << "  By assignment\t\t\t\t" << s2 << "\n";
+		const char* c = "Hello std::string 8";
+		stringstream myStream;
+		myStream << c;
+		string s = myStream.str();
+		cout << "  Using std::stringstream\t\t" << s << "\n";
 	}
 	cout << endl;
 }
