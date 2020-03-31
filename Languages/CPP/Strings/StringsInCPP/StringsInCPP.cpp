70a4b4f4b1b397ea038f75eab54196e8c9c9d883
Alain Dekker <abdekker.online@gmail.com>
2020-03-31 10:25:15 +0200

    Basic string manipulations in C++

---
+++ Languages/CPP/Strings/StringsInCPP/StringsInCPP.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+using namespace std;
+
+void ConstructStdString()
+{
+	// Various ways to construct a std::string
+	cout << "Construct a std::string\n";
+
+	{
+		string s = "Hello std::string 1";
+		cout << "  Operator= with string literal\t\t" << s << "\n";
+	}
+
+	{
+		string s("Hello std::string 2");
+		cout << "  From string literal\t\t\t" << s << "\n";
+	}
+
+	{
+		const char* c = "Hello std::string 3a";
+		string s(c);
+		cout << "  From char*\t\t\t\t" << s << "\n";
+	}
+
+	{
+		const char* c = "Hello std::string 3b";
+		size_t len = strlen(c);
+		string s(c, len);		// Use "len-2" (eg) to remove the last two characters
+		cout << "  From char*\t\t\t\t" << s << "\n";
+	}
+
+	{
+		const char* c = "Hello std::string 3c";
+		string s = c;	// Direct assignment
+		cout << "  From char*\t\t\t\t" << s << "\n";
+	}
+
+	{
+		string s;
+		const char* c = "Hello std::string 4";
+		size_t len = strlen(c);
+		s.assign(c, len);	// Only if the string already exists
+		cout << "  Using std::string::assign\t\t" << s << "\n";
+	}
+
+	{
+		const char* c1 = "Hello ";
+		const char* c2 = "std::string 5"; 
+		string s = string(c1) + string(c2);
+		cout << "  Using std::string::operator+\t\t" << s << "\n";
+	}
+
+	{
+		string s1 = "Hello std::string 6";
+		string s2 = s1;
+		cout << "  By assignment\t\t\t\t" << s2 << "\n";
+	}
+	cout << endl;
+}
+
+void ConstructCharPointers()
+{
+	// Various ways to construct a char* from std::string
+	cout << "Construct a char* from a std::string\n";
+
+	{
+		string s = "Hello char* 1a";
+		char c[20];
+		strcpy_s(c, s.c_str());
+		printf("  Using std::string::c_str\t\t%s\n", c);
+	}
+
+	{
+		string s = "Hello char* 1b";
+		char c[20];
+		strcpy_s(c, &s[0]);
+		printf("  Using std::string::c_str\t\t%s\n", c);
+	}
+
+	{
+		string s = "Hello char* 2";
+		char c[20];
+		s.copy(c, s.max_size() + 1);
+		c[s.size()] = '\0';
+		printf("  Using std::string::copy\t\t%s\n", c);
+	}
+
+	{
+		string s = "Hello char* 3";
+		char c[20];
+		std::copy(s.begin(), s.end(), c);
+		c[s.size()] = '\0';
+		printf("  Using std::copy\t\t\t%s\n", c);
+	}
+	cout << endl;
+}
+
+int main()
+{
+    cout << "Simple string manipulations in C++\n\n";
+
+	// Construct a std::string
+	ConstructStdString();
+
+	// Construct char* from std::string
+	ConstructCharPointers();
+}
