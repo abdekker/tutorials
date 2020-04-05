a4428d8b1f8299da82c97e9ef30b57682bae8a12
Alain Dekker <abdekker.online@gmail.com>
2020-04-05 16:38:57 +0200

    Demonstrates counting files in a folder

---
+++ CommandLine/Batch-Windows/CountFiles.bat
@@ -0,0 +1,31 @@
+@echo off
+
+rem This script demonstrates simple counting of files in a folder
+
+echo ### Count files in current folder ###
+rem Using "for /f" with pipe (|) command. This searches for the line "X File(s)" when calling "dir".
+echo Using "for /f" with pipe (^|) looking for "X File(s)" when calling the "dir" command
+for /f %%a in ('dir ^| find "File(s)"') do set cnt=%%a
+echo   Count (current folder) = %cnt%
+for /f %%a in ('dir %WINDIR% ^| find "File(s)"') do set cnt=%%a
+echo   Count (all files in %WINDIR%) = %cnt%
+for /f %%a in ('dir %WINDIR%\*.log ^| find "File(s)"') do set cnt=%%a
+echo   Count (log files only in %WINDIR%) = %cnt%
+echo .
+
+echo ### Count of all files in %WINDIR% (alternative syntax) ###
+echo Using "for /f" with "dir /b" (bare format) and looking for non-empty lines
+rem The "/a-d-s-h" ensures that only non-hidden and non-system files are returned
+for /f %%a in ('dir %WINDIR% /a-d-s-h /b ^| find /v /c ""') do set cnt=%%a
+echo   Count (all files in %WINDIR%) = %cnt%
+echo .
+
+echo ### Iterate through all files, incrementing a variable ###
+echo Simple iteration (which counts hidden and system files too so may be different)
+rem Use "for %%a in (*) do set /a cnt+=1" for the current folder
+set cnt=0
+for %%a in (%WINDIR%\*) do set /a cnt+=1
+echo   Count (all files in %WINDIR%) = %cnt%
+echo .
+
+echo All done!
