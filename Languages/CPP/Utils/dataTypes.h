7b38f8e359354461248854f6ad283d6bc3be3cd6
Alain Dekker <abdekker.online@gmail.com>
2020-04-08 18:03:36 +0200

    Data types and structures

---
+++ Languages/CPP/Utils/dataTypes.h
@@ -0,0 +1,47 @@
+#pragma once
+
+// Data types and structure definitions
+
+// Type definitions
+typedef std::vector<int> ARRAY;
+typedef std::vector< std::vector<int>> MATRIX;
+
+
+// Data structures
+
+// Simple (x,y) point. Usage example: POINT<int> location = {1, 2};
+template <typename T>
+struct POINT {
+	// Data
+	T x, y;
+
+	// Construction
+	POINT abs() {
+		POINT newPt;
+		newPt.x = (this->x > 0) ? this->x : -this->x;
+		newPt.y = (this->y > 0) ? this->y : -this->y;
+		return newPt;
+	}
+
+	// Operators
+	void operator= (const POINT &pt) { this->x = pt.x; this->y = pt.y; }
+
+	void operator+ (const POINT &pt) { this->x += pt.x; this->y += pt.y; }
+	void operator+= (const POINT &pt) { this->x += pt.x; this->y += pt.y; }
+
+	void operator- (const POINT &pt) { this->x -= pt.x; this->y -= pt.y; }
+	void operator-= (const POINT &pt) { this->x -= pt.x; this->y -= pt.y; }
+
+	void operator* (const T factor) { this->x *= factor; this->y *= factor;}
+	void operator*= (const T factor) { this->x *= factor; this->y *= factor; }
+
+	bool operator< (const POINT &pt) { return ((this->x < pt.x) || (this->y < pt.y)); }
+	bool operator> (const POINT &pt) { return ((this->x > pt.x) || (this->y > pt.y)); }
+
+	// Transformations
+	void transformSwap() { T tmp = this->x; this->x = this->y; this->y = tmp; }
+	void transformAbs() {
+		this->x = (this->x >= 0) ? this->x : -this->x;
+		this->y = (this->y >= 0) ? this->y : -this->y;
+	}
+};
