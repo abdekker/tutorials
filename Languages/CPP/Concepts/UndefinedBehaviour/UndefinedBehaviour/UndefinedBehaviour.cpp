71031632d57315d52b761d9abd53b9d044ddd1ae
Alain Dekker <alain@papyrusauthor.com>
2020-06-09 13:38:08 +0200

    Example with adding 1 to the largest representable integer

--- Languages/CPP/Concepts/UndefinedBehaviour/UndefinedBehaviour/UndefinedBehaviour.cpp
+++ Languages/CPP/Concepts/UndefinedBehaviour/UndefinedBehaviour/UndefinedBehaviour.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <conio.h>
+#include <limits.h>
 
 using namespace std;
 
@@ -9,11 +10,23 @@ int main()
  
     // Shift-left operator
     cout << "### Example 1: Shift-left operator ###\n";
+    cout << "  [MSVC compiler warning C4293]\n";
     int myNumber = 1;
     cout << "  MyNumber = " << myNumber << endl;
-    cout << "  Now shift left (1 << 32). This should generate a compiler warning.\n";
-    myNumber = (1 << 32);   // warning C4293 [MSVC: shift count negative or too big, undefined behavior]
-    cout << "  MyNumber = " << myNumber << endl;
+    cout << "  Now shift left (X << 32)...this generates a compiler warning.\n";
+    myNumber = (1 << 32);           // warning C4293 [MSVC: shift count negative or too big, undefined behavior]
+    cout << "  MyNumber = " << myNumber << endl << endl;
+
+    // Largest representable integer
+    cout << "### Example 2: Adding 1 to the largest representable integer ###\n";
+    cout << "  [MSVC compiler warning C4307]\n";
+    cout << "  INT_MAX (the largest representable integer) = " << INT_MAX << endl;
+    cout << "  Is (INT_MAX + 1) positive or negative?";
+    myNumber = (INT_MAX + 1);       // warning C4307 [MSVC: signed integral constant overflow]
+    if (myNumber < 0)
+        cout << " Negative (" << myNumber << ")!\n";
+    else
+        cout << " Non-negative (" << myNumber << ")!\n";
  
     // Pause the program
     _getch();
