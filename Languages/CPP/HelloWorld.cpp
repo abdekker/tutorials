864ed1f89570ca944b2256fc79efa9b4d4ec495a
Alain Dekker <alain@papyrusauthor.com>
2020-06-15 12:25:31 +0200

    Simple program to show command-line compilation

---
+++ Languages/CPP/HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Sample program which can be compiled from the command-line.
+    - Windows (Visual Studio): TODO
+    - Linux Ubuntu (gcc): gcc HelloWorld.cpp -o hw.out
+        - More generally "gcc FILE1 FILE2 ... -o OUTPUT"
+        - Run with "./hw.out"
+*/
+int main(int argc, char* args[])
+{
+    printf("Hello, world!\n");
+    return 0;
+}
