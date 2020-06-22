f7af10c8bd62c2f5124e1d9665a144028791cf29
Alain Dekker <alain@papyrusauthor.com>
2020-06-22 16:21:25 +0200

    Update with information about git commands

--- Languages/CPP/QuickTests.cpp
+++ Languages/CPP/QuickTests.cpp
@@ -4,12 +4,28 @@
 #include <string>
 
 // This dummy file has been added to the Git repo but is intended for quick tests of C/C++ only.
-// To keep the file in the index, but tell Git to start ignoring changes, use this command:
-//      $ git update-index --assume-unchanged path/to/file
-// To start tracking changes again, use this command:
-//      $ git update-index --no-assume-unchanged path/to/file
 
-// Adapted from: https://stackoverflow.com/questions/6964297/untrack-files-from-git-temporarily
+/* To keep this file in the index, but tell Git to start ignoring changes, there are two commands:
+    $ git update-index --assume-unchanged QuickTests.cpp (or)
+    $ git update-index --skip-worktree QuickTests.cpp
+
+These two commands work similarly but have different use-cases.
+* assume-unchanged is designed for cases where it is expensive to check whether the file has been
+    modified. This flag assumes the developer shouldn't change the file, and is intended primarily
+    for improving performance (eg. for not-changing folders like SDKs).
+* skip-worktree instructs Git not to touch the file and is intended for situations where the
+    developers should change, or are expected to change, the file
+
+Generally, "skip-worktree" is the option you should use.
+
+To start tracking changes again, use the appropriate (anti-)command:
+    $ git update-index --no-assume-unchanged QuickTests.cpp (or)
+    $ git update-index --no-skip-worktree QuickTests.cpp
+
+Adapted from:
+- https://stackoverflow.com/questions/6964297/
+- https://stackoverflow.com/questions/13630849/
+- https://git-scm.com/docs/git-update-index */
 
 using namespace std;
 int main(int argc, char* args[])
