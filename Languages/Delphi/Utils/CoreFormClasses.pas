cd8d1d4e58b18186d74ef7eaef20469bfc016f02
Alain Dekker <abdekker.online@gmail.com>
2020-06-28 22:34:02 +0200

    New rectangled class derived from TImage

--- Languages/Delphi/Utils/CoreFormClasses.pas
+++ Languages/Delphi/Utils/CoreFormClasses.pas
@@ -5,9 +5,10 @@ unit CoreFormClasses;
 interface
 
 uses
-  Windows, Classes, Controls, Forms;
+  Windows, Classes, Controls, ExtCtrls, Forms, Graphics;
 
 type
+  // General base form
   TGeneralBaseForm = class(TForm)
   private
 	{ Private declarations }
@@ -32,15 +33,43 @@ type
   end;
   TGeneralBase = class of TGeneralBaseForm;
 
+  // Sub-class of TImage to show a selected rectangle
+  TRectangleImage = class(TImage)
+  private
+	// Rectangles can be selected or (optionally) not selected
+	m_bShowSelected, m_bShowNotSelected: Boolean;
+	m_tSelected, m_tNotSelected: TColor;
+
+	procedure SetShowSelected(bShowSelected: Boolean);
+	procedure SetShowNotSelected(bShowNotSelected: Boolean);
+
+  protected
+	procedure Paint(); override;
+
+  public
+	constructor Create(AOwner: TComponent); override;
+
+	property SetSelectedColour: TColor read m_tSelected write m_tSelected;
+	property SetNotSelectedColour: TColor read m_tNotSelected write m_tNotSelected;
+
+	property ShowSelected: Boolean read m_bShowSelected write SetShowSelected;
+	property ShowNotSelected: Boolean read m_bShowNotSelected write SetShowNotSelected;
+  end;
+
 implementation
 
 uses
-  Graphics, StdCtrls;
+  StdCtrls;
 
 const
   // Maximum level of nesting for iterating child controls (eg. setting character set)
   MAX_CHILD_CONTROL_ITERATIONS = 8;
 
+type
+  TGraphicControlAccess = class(TGraphicControl)
+  end;
+
+// ### Start: TGeneralBaseForm ###
 // Public functions: Start
 constructor TGeneralBaseForm.Create(AOwner: TComponent);
 begin
@@ -61,7 +90,7 @@ end;
 procedure TGeneralBaseForm.SetComboHandlers(control: TControl);
 begin
 	// Set up the OnDropDown and OnCloseUp handlers for a combobox
-	// Note: See "SetChildComboHandlers" to iteratively set handlers for container controls (such
+	// Note: Use "SetChildComboHandlers" to iteratively set handlers for container controls (such
 	// as a groupbox) which have many child combo or group controls
 	TComboBox(control).OnDropDown := ddlComboDropDown;
 	TComboBox(control).OnCloseUp := ddlComboCloseUp;
@@ -107,6 +136,63 @@ begin
 	TComboBox(Sender).Color := clInfoBk;
 end;
 // Private functions: End
+// ### End: TGeneralBaseForm ###
+
+// ### Start: TRectangleImage ###
+constructor TRectangleImage.Create(AOwner: TComponent);
+begin
+	// Call base constructor
+	inherited;
+
+	// Initialise flags
+	m_bShowSelected := False;
+	m_bShowNotSelected := False;
+	m_tSelected := clRed;
+	m_tNotSelected := clWhite;
+end;
+
+procedure TRectangleImage.SetShowSelected(bShowSelected: Boolean);
+begin
+	if (m_bShowSelected <> bShowSelected) then
+		begin
+		m_bShowSelected := bShowSelected;
+		Invalidate();
+		end;
+end;
+
+procedure TRectangleImage.SetShowNotSelected(bShowNotSelected: Boolean);
+begin
+	if (m_bShowNotSelected <> bShowNotSelected) then
+		begin
+		m_bShowNotSelected := bShowNotSelected;
+		Invalidate();
+		end;
+end;
+
+procedure TRectangleImage.Paint();
+begin
+	inherited;
+	if (m_bShowSelected) then
+		begin
+		with TGraphicControlAccess(Self).Canvas do
+			begin
+			Brush.Style := bsClear;
+			Pen.Color := m_tSelected;
+			Rectangle(ClientRect);
+			end;
+		end
+	else if (m_bShowNotSelected) then
+		begin
+		with TGraphicControlAccess(Self).Canvas do
+			begin
+			Brush.Style := bsClear;
+			Pen.Color := m_tNotSelected;
+			Rectangle(ClientRect);
+			end;
+		end;
+end;
+
+// ### End: TRectangleImage ###
 
 end.
 
