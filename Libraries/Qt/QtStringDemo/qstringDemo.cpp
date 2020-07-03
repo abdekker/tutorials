d205bf3577aa41f42328712c5cd227da2eb7d02a
Alain Dekker <alain@papyrusauthor.com>
2020-07-03 15:51:52 +0200

    Output using QTextStream

--- Libraries/Qt/QtStringDemo/qstringDemo.cpp
+++ Libraries/Qt/QtStringDemo/qstringDemo.cpp
@@ -11,19 +11,21 @@ The default "Qt Console Application" wizard adds these lines and include. Not su
 "CoreApplication" is or how to use it.
     //#include <QtCore/QCoreApplication> (this would be at the top)
     //QCoreApplication a(argc, argv);
-    //return a.exec();
-*/
+    //return a.exec(); */
 
 inline void qStdout(const QString &output)
 {
     QTextStream r{stdout};
     r << output;
-    r.flush();
 }
 
 using namespace std;
 int main(int argc, char *argv[])
 {
+    // Ensure the output buffer is flushed on each insertion operation
+    setvbuf(stdout, NULL, _IONBF, 0);
+    //std::cout << std::unitbuf;
+
     // Demonstrates some aspects of using QString
     // You must install the Qt library and the "Qt VS Tools" extension to build in VS 2019
     cout << "Using QString (and Qt debugging) from the Qt library\n";
@@ -31,19 +33,17 @@ int main(int argc, char *argv[])
 
     cout << "### Output to the console ###\n";
     cout << "  Hello world using std::cout\n";
-    qDebug() << "  Hello world using qDebug. Newline character is not required.";
-    QString sHelloWorld(" Hello world using qDebug with a QString. Quotation marks are included.");
-    qDebug() << sHelloWorld;
+    qDebug() << "  Hello world using qDebug with char*. Newline character is not required.";
+    QString sHW1(" Hello world using qDebug with a QString. Quotation marks are included.");
+    qDebug() << sHW1;
+    QString sHW2 = QString("  Hello world using QTextStream\n");
+    qStdout(sHW2);
 
-    cout << "\nAlternate ways to output\n";
+    cout << "\nAlternate ways to output QString\n";
     QString sTest("donkey");
     qDebug() << "  qDebug: " << sTest;
     cout << "  cout: " << sTest.toLatin1().data() << " (using QString::toLatin1::data)\n";
     cout << "  cout: " << sTest.toUtf8().data() << " (using QString::toUtf8::data)\n";
-
-    // QTextStream captures the standard output. How to get it to release it?
-    //sHW = QString("  Hello world using QTextStream. Quotation marks are now NOT included.\n");
-    //qStdout(sHW);
 
     cout << "\nThere are four Qt debugging options: qDebug, qWarning, qCritical and qFatal\n";
     qDebug() << "  Debug Message (may be removed at compile time)";
