1da8ecada88a978ca88dc784f5ac710eb11389fb
Alain Dekker <alain@papyrusauthor.com>
2020-07-09 10:21:08 +0200

    Adding 2nd algorithm for counting bits

--- Languages/CPP/Utils/mathHelper.h
+++ Languages/CPP/Utils/mathHelper.h
@@ -2,23 +2,35 @@
 
 // Header-only helper class for some maths and computer science tricks
 using namespace std;
+
+// General purpose maths class
 class mathHelper
 {
 public:
-    mathHelper()
+    // Constructor / destructor
+    mathHelper() {}
+    ~mathHelper() {}
+
+    // Count bits set
+    int CountBitsSet_U1(unsigned int input)
     {
-        // Constructor
+        // Known as the 'parallel' or 'variable-precision SWAR' algorithm. This code was taken
+        // from the accepted answer to: https://stackoverflow.com/questions/109023
+        input = (input - ((input >> 1) & 0x55555555));
+        input = (input & 0x33333333) + ((input >> 2) & 0x33333333);
+        return ((((input + (input >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24);
     }
 
-    ~mathHelper()
+    int CountBitsSet_U2 (unsigned int input)
     {
-        // Destructor
-    }
+        // More readable version (which won't be as quick, though)
+        int bits = 0;
+        while (input > 0) {           // repeat until all bits are zero
+            if ((input & 1) == 1)     // check lower bit
+                bits++;
 
-    int BitsSet_UL(unsigned long input)
-    {
-	    input = (input - ((input >> 1) & 0x55555555));
-	    input = (input & 0x33333333) + ((input >> 2) & 0x33333333);
-	    return ((((input + (input >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24);
+            input >>= 1;               // shift bits, removing lower bit
+        }
+        return bits;
     }
 };
