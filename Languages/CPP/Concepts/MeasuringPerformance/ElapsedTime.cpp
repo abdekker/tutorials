e5d904feec0435d2bfacf91b032c0c0189ce2324
Alain Dekker <alain@papyrusauthor.com>
2020-07-17 16:50:47 +0200

    Technique using clock()

--- Languages/CPP/Concepts/MeasuringPerformance/ElapsedTime.cpp
+++ Languages/CPP/Concepts/MeasuringPerformance/ElapsedTime.cpp
@@ -6,19 +6,35 @@ int main()
 {
     // Send output to console
     setvbuf(stdout, NULL, _IONBF, 0);
-    std::cout << "Measuring performance using std::chrono\n\n";
+    std::cout << "Measuring performance using std::chrono\n";
 
     // Measure some time-consuming operation
     std::chrono::system_clock::time_point tmp;
-    auto startTime = std::chrono::system_clock::now();
+    auto start1 = std::chrono::system_clock::now();
 
     // Some computation here...
     for (int loop=0; loop < 100000000; loop++)
         tmp = std::chrono::system_clock::now();
 
     // Show results
-    std::chrono::duration<double> elapsedSeconds = (std::chrono::system_clock::now() - startTime);
-    std::cout << "Elapsed time: " << elapsedSeconds.count() << "s\n";
-    std::cout << "All done!\n";
+    std::chrono::duration<double> elapsed1 = (std::chrono::system_clock::now() - start1);
+    std::cout << "Elapsed time (std::chrono):\t" << elapsed1.count() << "s\n";
+
+    // Now use the time.h::clock method which returns the ms since the process started
+    std::cout << "\nMeasuring performance using time.h::clock()\n";
+
+    // Measure some time-consuming operation
+    clock_t start2 = clock();
+
+    // Some computation here...
+    for (int loop=0; loop < 100000000; loop++)
+        tmp = std::chrono::system_clock::now();
+
+    // Show results
+    clock_t elapsed2 = (clock() - start2);
+    std::cout << "Elapsed time (time.h::clock()):\t" << elapsed2 << "ms\n";
+
+    // Exit
+    std::cout << "\nAll done!\n";
     return 0;
 }
