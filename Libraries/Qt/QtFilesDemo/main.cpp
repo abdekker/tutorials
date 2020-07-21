f4bf26cef33b217c0d324f105f05b255cceba0b7
Alain Dekker <alain@papyrusauthor.com>
2020-07-21 18:16:09 +0200

    Files demo for Qt

--- Libraries/Qt/QtFilesDemo/main.cpp
+++ Libraries/Qt/QtFilesDemo/main.cpp
@@ -1,5 +1,6 @@
 #include <conio.h>
 #include <iostream>
+#include <algorithm>
 
 #include <QtCore/QCoreApplication>
 #include <QDir>
@@ -7,7 +8,30 @@
 
 #include "..\Utils\qstringHelper.h"
 
+// Example files (avoid deleting these)
+#define EXAMPLE_FILE_EMPTY      "ExampleEmpty.xyz"
+#define EXAMPLE_FILE_ICO        "ExampleICO.ico"
+#define EXAMPLE_FILE_TXT        "ExampleTXT.txt"
+
 using namespace std;
+QString CleanAppPath(QCoreApplication &a)
+{
+    // Helper function that "cleans the application" path, removing folders reported during debugging. For
+    // example, run the application directly and QCoreApplication::applicationDirPath is something like:
+    //      <DEV-PAT>\QtFilesDemo
+    // But now run it from the debugger, and the path will be similar to this:
+    //      <DEV-PAT>\QtFilesDemo\x64\Debug
+
+    // This method returns the former.
+    QString fullPath = a.applicationDirPath();
+    int posAppName = fullPath.indexOf(a.applicationName());
+    QString cleanPath = (posAppName != -1)
+        ? fullPath.mid(0, posAppName + a.applicationName().length())
+        : fullPath;
+
+    return cleanPath;
+}
+
 void TestQCoreApplication(QCoreApplication &a)
 {
     cout << "\n### QCoreApplication ###\n";
@@ -17,6 +41,7 @@ void TestQCoreApplication(QCoreApplication &a)
     // use it, so this method demonstrates some minor features of the object.
     cout << "  Application name:\t" << a.applicationName().toLatin1().data() << endl;
     cout << "  App dir path:\t\t" << a.applicationDirPath().toLatin1().data() << endl;
+    cout << "  App \"clean\" path:\t" << CleanAppPath(a).toLatin1().data() << endl;
     cout << "  App process ID:\t" << a.applicationPid() << endl;
     cout << "  App version:\t\t" << a.applicationVersion().toLatin1().data() << endl;
     cout << "  App object name:\t" << a.objectName().toLatin1().data() << endl;
@@ -46,6 +71,13 @@ void TestQDir()
     if (dir.cd(qgetenv("windir"))) {
         cout << "  Directory path:\t" << dir.absolutePath().toLatin1().data() << endl;
         cout << "  There are " << dir.count() << " files and folders in this directory (not recursive)\n";
+
+        QFileInfoList listDirs = dir.entryInfoList(QDir::Filter::Dirs, QDir::SortFlag::Name);
+        cout << "  There are " << listDirs.count() << " folders (not recursive). First few are:\n";
+        for (int dirNum = 0; dirNum < std::min(5, listDirs.count()); dirNum++) {
+            if (!listDirs[dirNum].fileName().contains('.'))
+                cout << "    " << listDirs[dirNum].fileName().toLatin1().data() << endl;
+        }
     }
     else
         cout << "  Failed to change path!\n";
@@ -59,37 +91,107 @@ void TestQDir()
     cout << "  Directory path:\t" << dir.absolutePath().toLatin1().data() << endl;
 
     QStringHelper helper;
-    QString sRandom = helper.GetRandomString(20);
-    cout << "(creating random folder \"" << sRandom.toLatin1().data() << "\"...";
-    if (dir.mkdir(sRandom))
+    QString sRandom1 = helper.GetRandomString(20);
+    cout << "  Check for random folder...";
+    if (dir.exists(sRandom1))
+        cout << "folder exists (oops!)\n";
+    else
+        cout << "folder does not exist as expected\n";
+
+    cout << "(creating random folder \"" << sRandom1.toLatin1().data() << "\"...";
+    if (dir.mkdir(sRandom1))
         cout << "done!)\n";
     else
         cout << "failed)\n";
 
-    dir.cd(sRandom);
+    dir.cd(sRandom1);
     if (dir.isEmpty())
         cout << "  Folder is empty as expected\n";
     else
         cout << "  Folder is not empty which is weird!\n";
 
     dir.cdUp();
-    cout << "(deleting random folder...";
-    if (dir.rmdir(sRandom))
+    QString sRandom2 = helper.GetRandomString(20);
+    cout << "(renaming random folder...";
+    if (dir.rename(sRandom1, sRandom2))
         cout << "done!)\n";
     else
         cout << "failed)\n";
 
-/*
-bool	remove(const QString &fileName)
-bool	removeRecursively()
-bool	rename(const QString &oldName, const QString &newName)
-*/
+    cout << "(deleting random folder...";
+    if (dir.rmdir(sRandom2))
+        cout << "done!)\n";
+    else
+        cout << "failed)\n";
+
+    // Other options include:
+    // * remove(const QString &fileName) - deletes the given file
+    // * bool removeRecursively() - removes the directory, including all contents
 
     cout << "#\n";
 }
 
-void TestReadWriteFiles()
+void TestQFile(QCoreApplication &a)
 {
+    cout << "\n### QFile ###\n";
+    cout << "(check for existence of example files)\n";
+
+    // Get the application path where example files (should) be located
+    QString appPath = (CleanAppPath(a) + QDir::separator());
+
+    // Generate a list of example files
+    QStringList examplesFileOnly, examplesFullPath;
+    examplesFileOnly.push_back(EXAMPLE_FILE_EMPTY);
+    examplesFileOnly.push_back(EXAMPLE_FILE_ICO);
+    examplesFileOnly.push_back(EXAMPLE_FILE_TXT);
+    for (auto example : examplesFileOnly)
+        examplesFullPath.push_back(appPath + example);
+
+    for (int fileNum=0; fileNum < examplesFullPath.size(); fileNum++) {
+        QString test = examplesFullPath[fileNum];
+        if (QFile::exists(examplesFullPath[fileNum]))
+            cout << "  \"" << examplesFileOnly[fileNum].toLatin1().data() << "\" exists!\n";
+        else
+            cout << "  \"" << examplesFileOnly[fileNum].toLatin1().data() << "\" does not exist\n";
+    }
+
+    cout << "\n(open each file and display a few bytes)\n";
+    FILE* f
+    QFile myFile;
+    QByteArray ba;
+    for (int fileNum=0; fileNum < examplesFullPath.size(); fileNum++) {
+        QFile::open(
+        myFile.open(examplesFullPath[fileNum], QIODevice::ReadOnly);
+        if (myFile.isOpen()) {
+            cout << "  \"" << examplesFileOnly[fileNum].toLatin1().data() << "\" has a size of " << myFile.size() << " bytes\n";
+            //myFile.read(c, 1
+        }
+
+        myFile.close();
+    }
+    //QFile myFile;
+    //QByteArray(
+    //char* c;
+    //
+    //myFile.open(examplesFullPath[0], QIODevice::ReadOnly);
+    //if (myFile.isOpen()) {
+    //    myFile.size();
+    //}
+    //while (!myFile.atEnd()) {
+    //    myFile.read(c, 1
+    //
+    //myFile.write(msg, qstrlen(msg));        // write to stderr
+    //file.close();
+    //
+    //
+
+    /*
+    // Example files (avoid deleting these)
+#define EXAMPLE_FILE_EMPTY      "ExampleEmpty.xyz"
+#define EXAMPLE_FILE_ICO        "ExampleICO.ico"
+#define EXAMPLE_FILE_TXT        "ExampleTXT.txt"
+    */
+    cout << "#\n";
 }
 
 int main(int argc, char *argv[])
@@ -99,7 +201,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     TestQCoreApplication(a);
     TestQDir();
-    TestReadWriteFiles();
+    TestQFile(a);
 
     // Exit
     a.quit();
