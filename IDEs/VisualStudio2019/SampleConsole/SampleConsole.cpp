7419bd1933bed597ee8ea3ef9461ea0c4f78c757
Alain Dekker <abdekker.online@gmail.com>
2020-11-18 14:06:32 +0000

    Conversion of tabs to spaces

--- IDEs/VisualStudio2019/SampleConsole/SampleConsole.cpp
+++ IDEs/VisualStudio2019/SampleConsole/SampleConsole.cpp
@@ -8,32 +8,32 @@
 
 void ShowArguments(int argc, char *argv[])
 {
-	// Output the number of the arguments
+    // Output the number of the arguments
     std::cout << "Number of arguments: " << argc << "\n";
-	if (argc > 0)
-	{
-		// One or more arguments to this console application. On Windows, the 1st argument should
-		// be the application name (including full path).
-		size_t	nLength = 0;
-		char	aszArguments[MAX_ARG][MAX_PATH];
-		for (BYTE byArg = 0; byArg < argc; byArg++)
-		{
-			nLength = strlen(argv[byArg]);
-			strncpy_s(aszArguments[byArg], argv[byArg], nLength);
-			aszArguments[byArg][nLength] = '\0';
+    if (argc > 0)
+    {
+       // One or more arguments to this console application. On Windows, the 1st argument should
+        // be the application name (including full path).
+        size_t	nLength = 0;
+        char	aszArguments[MAX_ARG][MAX_PATH];
+        for (BYTE byArg = 0; byArg < argc; byArg++)
+        {
+            nLength = strlen(argv[byArg]);
+            strncpy_s(aszArguments[byArg], argv[byArg], nLength);
+            aszArguments[byArg][nLength] = '\0';
             std::cout << "Arg " << (short)byArg << " = " << aszArguments[byArg] << "\n";
-		}
+       }
 
-		std::cout << "\n";
-	}
+        std::cout << "\n";
+    }
 }
 
 int main(int argc, char *argv[])
 {
     printf("Visual Studio 2019 sample console application...\n\n");
-	ShowArguments(argc, argv);
+    ShowArguments(argc, argv);
 
-	// Prompt for exit (holds application on-screen if launched outside the IDE)
-	std::cout << "Finished...press a key to exit\n";
-	_getch();
+    // Prompt for exit (holds application on-screen if launched outside the IDE)
+    std::cout << "Finished...press a key to exit\n";
+    _getch();
 }
