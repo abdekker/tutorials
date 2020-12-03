6a9b73bdbd72fce161d1e7308083eb86882cef3d
Alain Dekker <abdekker.online@gmail.com>
2020-12-03 19:07:44 +0000

    C# project which generates library code

---
+++ Languages/C#/Utils/systemHelper/systemHelperLibrary/SystemLibrary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace systemHelperLibrary
+{
+    // This library project was original adapted from:
+    // https://docs.microsoft.com/en-us/dotnet/core/tutorials/library-with-visual-studio
+    public static class SystemLibrary
+    {
+        public static bool Is64Bit()
+        {
+            // Is this process 64-bit or 32-bit? There are two good methods in C#.
+            // Method 1: Use Environment.Is64BitProcess
+            // Method 2: Check the size of IntPtr
+            return (IntPtr.Size == 8);
+        }
+    }
+}
