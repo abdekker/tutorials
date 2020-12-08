70e2c4da3ce2004e6f8ba455331dae5839e7727c
Alain Dekker <abdekker.online@gmail.com>
2020-12-08 11:04:42 +0000

    Comments about how the class library was created

--- Languages/C#/Utils/systemHelper/systemHelperLibrary/SystemLibrary.cs
+++ Languages/C#/Utils/systemHelper/systemHelperLibrary/SystemLibrary.cs
@@ -4,6 +4,17 @@ namespace systemHelperLibrary
 {
     // This library project was original adapted from:
     // https://docs.microsoft.com/en-us/dotnet/core/tutorials/library-with-visual-studio
+
+    /* To create a C# Class Library (DLL)
+        - Create new project
+        - Set language to "C#"
+        - Enter "solution" in search
+        - Select "Blank solution"
+        - Right-click solution in Solution Explorer
+        - Add > New Project
+        - Enter "library" in search
+        - Select "Class Library (.NET Framework)" (Windows only)
+    */
     public static class SystemLibrary
     {
         public static bool Is64Bit()
