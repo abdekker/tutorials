0e8ef0212325f47e7bdc241ac7cdf186c40a5c39
Alain Dekker <abdekker.online@gmail.com>
2020-12-16 13:23:40 +0000

    Minor update to assembly information

--- Languages/C#/Utils/systemHelper/systemHelperLibrary/Properties/AssemblyInfo.cs
+++ Languages/C#/Utils/systemHelper/systemHelperLibrary/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("systemHelperLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Helper methods written in C#")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DekkerSoft")]
 [assembly: AssemblyProduct("systemHelperLibrary")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © DekkerSoft")]
+[assembly: AssemblyTrademark("DekkerSoft")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
