01eb0d7af3bd1185f94e45760feaa46bf6adeac7
Alain Dekker <abdekker.online@gmail.com>
2021-01-11 21:48:54 +0000

    Method added to format floating point numbers

--- Languages/C#/Utils/systemHelper/systemHelperLibrary/StringLibrary.cs
+++ Languages/C#/Utils/systemHelper/systemHelperLibrary/StringLibrary.cs
@@ -2,14 +2,25 @@
 {
     public static class StringLibrary
     {
-        public static bool StartsWithUpper(this string str)
+        public static bool StartsWithUpper(this string s)
         {
             // "Help" returns true, and "help" returns false.
-            if (string.IsNullOrWhiteSpace(str))
+            if (string.IsNullOrWhiteSpace(s))
                 return false;
 
-            char ch = str[0];
+            char ch = s[0];
             return char.IsUpper(ch);
+        }
+
+        public static string FormatFloat<T>(T value, int precision)
+        {
+            // Generic method to format a floating point number
+            // Note: String interpolation replaces double braces "{{" with a single brace
+            return string.Format($"{{0:F{precision}}}", value);
+
+            // Alternative:
+            /*string formatString = string.Concat("{0:F", precision, "}");
+            return string.Format(formatString, value);*/
         }
     }
 }
