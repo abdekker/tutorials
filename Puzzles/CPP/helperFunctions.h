20fd8af254b592c69c59a8612a0eba9b9816ac3c
Alain Dekker <abdekker.online@gmail.com>
2021-01-11 02:27:14 +0000

    Include guards

--- Puzzles/CPP/helperFunctions.h
+++ Puzzles/CPP/helperFunctions.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef HELPER_FUNTIONS_H
+#define HELPER_FUNTIONS_H
 #include "stdafx.h"
 
 // Helper functions to use with the puzzles (generally not related to the puzzles themselves)
@@ -7,3 +8,4 @@ void PrintArray(ARRAY input, const int cWidth, const char cFill = ' ', const cha
 
 void PrintMatrix(MATRIX input, const char cSeparator = ' ');
 void PrintMatrix(MATRIX input, const int cWidth, const char cFill = ' ', const char cSeparator = ' ');
+#endif //HELPER_FUNTIONS_H
