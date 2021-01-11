2bd6342e15561c1738ffd49943231aec34577960
Alain Dekker <abdekker.online@gmail.com>
2021-01-11 00:09:52 +0000

    Removing method for printing a vector

--- Languages/CPP/Utils/stringHelper.h
+++ Languages/CPP/Utils/stringHelper.h
@@ -1,8 +1,6 @@
 #pragma once
 
 #include <cstring>
-#include <iterator>
-#include <vector>
 
 // Header-only helper class for using strings
 // Note: It is bad practice to include something like "using namespace X" in header files, or
@@ -37,37 +35,6 @@ public:
         }
         else
             return std::string(); // Empty std::string
-    }
-
-    template <class T>
-    void PrintVector(std::vector<T> input, const char cSeparator = ' ', bool bNewLine = false)
-    {
-        // Output an array (std::vector) to the console eg. {1,11,21} => 1 11 21.
-        // Example: stringHelper::PrintVector<int>(vecOfInts);
-        if (input.size() > 0)
-        {
-            // This does not compile in VS 2019 (reported to MS in Jan 2021)
-            //      for (std::vector<T>::iterator it = input.begin(); it != input.end();)
-
-            for (auto it = input.begin(); it != input.end();)
-            {
-                std::cout << *it;
-                if (++it != input.end())
-                    std::cout << cSeparator;
-            }
-
-            // Alternatively, use a range-based loop using auto. This has the disadvantage of not
-            // being easy to identify specific elements (such as the last):
-            /*for (auto a : input)
-            {
-                std::cout << a << cSeparator;
-            }*/
-
-            if (bNewLine)
-                std::cout << std::endl;
-        }
-        //else
-        //    std::cout << "(empty)\n";
     }
 
     // Non-template functions
