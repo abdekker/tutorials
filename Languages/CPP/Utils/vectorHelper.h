d3b22bd4f8669cc4732fc9c78d08a293d5f0777e
Alain Dekker <abdekker.online@gmail.com>
2021-01-11 00:10:12 +0000

    Header-only file for working with vectors

---
+++ Languages/CPP/Utils/vectorHelper.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <vector>
+
+// Header-only helper class for using vectors
+class vectorHelper
+{
+public:
+    // Constructor / destructor
+    vectorHelper() {}
+    ~vectorHelper() {}
+
+    template <typename T> 
+    const bool Contains(std::vector<T>& vec, const T& elem)
+    {
+        // Does the vector contains the requested value?
+        if (std::find(vec.begin(), vec.end(), elem) != vec.end())
+            return true;
+
+        return false;
+    }
+
+    template <typename T>
+    void PrintVector(std::vector<T> input, const char cSeparator = ' ', bool bNewLine = false)
+    {
+        // Output an array (std::vector) to the console eg. {1,11,21} => 1 11 21.
+        // Example: stringHelper::PrintVector<int>(vecOfInts);
+        if (input.size() > 0)
+        {
+            // This does not compile in VS 2019 (reported to MS in Jan 2021)
+            //      for (std::vector<T>::iterator it = input.begin(); it != input.end();)
+            for (auto it = input.begin(); it != input.end();)
+            {
+                std::cout << *it;
+                if (++it != input.end())
+                    std::cout << cSeparator;
+            }
+
+            // Alternatively, use a range-based loop using auto. This has the disadvantage of not
+            // being easy to identify specific elements (such as the last):
+            /*for (auto a : input)
+            {
+                std::cout << a << cSeparator;
+            }*/
+
+            if (bNewLine)
+                std::cout << std::endl;
+        }
+        //else
+        //    std::cout << "(empty)\n";
+    }
+};
