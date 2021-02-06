f7313157d92347e6f065de0a2ec5a8c3bfa22c6e
Alain Dekker <abdekker.online@gmail.com>
2021-02-06 23:40:32 +0000

    How to pause execution after the script has completed

---
+++ CommandLine/PowerShell-Windows/DontCloseAfterExecution.ps1
@@ -0,0 +1,14 @@
+﻿<# There are a few options to stop the script exiting after executiong:
+1) From the command prompt run: PowerShell -NoExit "Path\to\Script\script.ps1"
+	(Does not close PowerShell after running the script; type "exit" to close Powershell)
+2a) Add to the end of the script: pause
+	(Displays "Press Enter to continue...:", similar to "pause" in a command prompt script)
+2b) Add to the end of the script: read-Host -Prompt "Press Enter to exit"
+	(Similar to "pause", but allows a customised message to be displayed)
+3) Add to the end of the script: $host.enternestedprompt()
+	(This creates a nested prompt. When the nested prompt is closed, then PowerShell will exit.)
+#>
+Write-Host "Hello, World!"
+# pause
+read-Host -Prompt "The script is completed...press a key to exit"
+# $host.enternestedprompt()
