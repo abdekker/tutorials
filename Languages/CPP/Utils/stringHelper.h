6126053788cf2a161e62d8e92e53e324efe71983
Alain Dekker <abdekker.online@gmail.com>
2021-02-10 16:32:20 +0000

    Method naming convention, randomised C++ strings

--- Languages/CPP/Utils/stringHelper.h
+++ Languages/CPP/Utils/stringHelper.h
@@ -1,6 +1,12 @@
 #pragma once
 
 #include <cstring>
+#include <algorithm>
+#include <random>
+#include <vector>
+
+#include "mathHelper.h"
+#include "vectorHelper.h"
 
 // Header-only helper class for using strings
 // Note: It is bad practice to include something like "using namespace X" in header files, or
@@ -14,7 +20,7 @@ public:
 
     // Variadic template functions (using C++11 or later)
     template <typename... Args>
-    std::string formatString(const char *format, Args... args)
+    std::string FormatString(const char *format, Args... args)
     {
         // Helper function to format a string
 
@@ -38,9 +44,78 @@ public:
     }
 
     // Non-template functions
-    bool containsSubString(const std::string &mainString, const std::string &subString)
+    bool ContainsSubString(const std::string &mainString, const std::string &subString)
     {
         // Example: containsSubString(string("fred"), string("red"));   // true
         return (mainString.find(subString) != std::string::npos);
+    }
+
+    std::string GetRandomString(uint8_t length, bool lettersOnly, bool removeIllegal = false)
+    {
+        // Generate a randomised string
+        std::vector<char> output;
+        mathHelper mHelper;
+        length = mHelper.clamp(length, (uint8_t)1, (uint8_t)100);
+
+        // Assign allowed characters to a temporary array
+        uint8_t start, end, character;
+        std::vector<char> allowed;
+        std::vector<char> forbidden;
+        if (lettersOnly)
+        {
+            // Lowercase letters only
+            start = 97;     // a
+            end = 122;      // z
+        }
+        else
+        {
+            // Any printable ASCII character
+            start = 33;     // !
+            end = 126;      // ~
+
+            // Remove illegal characters? Use if the random string will be used to create a file/folder in
+            // the file system. Forbidden printable ASCII characters are:
+            // * Windows:   \ / : * ? " < > |
+            // * MacOS:     :
+            // * Linux:     /
+            if (removeIllegal)
+            {
+                // Use the Windows superset because it covers MacOS and Linux too
+                forbidden.push_back('\\');
+                forbidden.push_back('/');
+                forbidden.push_back(':');
+                forbidden.push_back('*');
+                forbidden.push_back('?');
+                forbidden.push_back('"');
+                forbidden.push_back('<');
+                forbidden.push_back('>');
+                forbidden.push_back('|');
+            }
+        }
+
+        if (forbidden.size() > 0)
+        {
+            vectorHelper vHelper;
+            for (character = start; character <= end; character++)
+            {
+                if (!vHelper.Contains(forbidden, (char)character))
+                    allowed.push_back((char)character);
+            }
+        }
+        else
+        {
+            for (character = start; character <= end; character++)
+                allowed.push_back((char)character);
+        }
+
+        // Create a random number generator (with optional seed for performance and test)
+        std::mt19937 rnd(std::random_device{}());
+        std::uniform_int_distribution<> dist(0, INT_MAX);   // Avoid constructing distribution all the time
+
+        // Build randomised string
+        while (output.size() < length)
+            output.push_back(allowed[dist(rnd) % allowed.size()]);
+
+        return std::string(output.begin(), output.end());
     }
 };
