979fdf264238e6e42a481b54cf8be165e8d2ea2e
Alain Dekker <abdekker.online@gmail.com>
2021-02-15 13:48:33 +0000

    How to list Windows System Apps

---
+++ CommandLine/PowerShell-Windows/ListWindowsApps.ps1
@@ -0,0 +1,9 @@
+﻿# Creates a list of the installed Windows Systems Apps (these come pre-installed with Windows 8+)
+
+# Update your username in the line below (and delete WindowsApps.txt, if necessary)
+Get-AppxPackage -User USERNAME > C:\Tmp\WindowsApps.txt
+
+# Review the list of installed applications
+# To uninstall an application run this command
+# $ Remove-AppxPackage PackageFullName
+# eg. Remove-AppxPackage Microsoft.Windows.OOBENetworkCaptivePortal
