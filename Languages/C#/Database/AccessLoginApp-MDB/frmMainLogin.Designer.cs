9a6b9fad53c1ad6d35927306c3e8db60ebf52481
Alain Dekker <abdekker.online@gmail.com>
2021-02-22 14:00:05 +0000

    Using a password mask character

--- Languages/C#/Database/AccessLoginApp-MDB/frmMainLogin.Designer.cs
+++ Languages/C#/Database/AccessLoginApp-MDB/frmMainLogin.Designer.cs
@@ -87,6 +87,7 @@
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(151, 32);
             this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
             // 
             // txtUsername
             // 
