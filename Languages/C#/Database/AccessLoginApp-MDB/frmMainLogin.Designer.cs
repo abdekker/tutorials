49f7818c658e3b142043419615336de8e2be853f
Alain Dekker <abdekker.online@gmail.com>
2021-02-22 01:34:27 +0000

    Tutorial for reading and writing to an Access database

---
+++ Languages/C#/Database/AccessLoginApp-MDB/frmMainLogin.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace AccessLoginApp_MDB
+{
+    partial class frmMainLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.groupLogin = new System.Windows.Forms.GroupBox();
+			this.lblStatus = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// groupLogin
+			// 
+			this.groupLogin.Location = new System.Drawing.Point(10, 15);
+			this.groupLogin.Name = "groupLogin";
+			this.groupLogin.Size = new System.Drawing.Size(600, 240);
+			this.groupLogin.TabIndex = 0;
+			this.groupLogin.TabStop = false;
+			this.groupLogin.Text = "Login Details";
+			// 
+			// lblStatus
+			// 
+			this.lblStatus.AutoSize = true;
+			this.lblStatus.Location = new System.Drawing.Point(12, 265);
+			this.lblStatus.Name = "lblStatus";
+			this.lblStatus.Size = new System.Drawing.Size(55, 13);
+			this.lblStatus.TabIndex = 1;
+			this.lblStatus.Text = "Status = ?";
+			// 
+			// frmMainLogin
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(619, 286);
+			this.Controls.Add(this.lblStatus);
+			this.Controls.Add(this.groupLogin);
+			this.Name = "frmMainLogin";
+			this.Text = "Main Logic (Access .mdb)";
+			this.Load += new System.EventHandler(this.frmMainLogin_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupLogin;
+		private System.Windows.Forms.Label lblStatus;
+	}
+}
+
---
+++ Languages/C#/Database/AccessLoginApp-MDB/frmMainLogin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.OleDb;
+
+using System.Windows.Forms;
+
+namespace AccessLoginApp_MDB
+{
+    // Following the tutorial (see below) using:
+    // * Microsoft Access (.mdb, pre Access 2007)
+    // * OleDb
+
+    // Tutorial URLs:
+    // 1) C# MS Access Database Tutorial 1 # Getting Started and Access database Connection (https://www.youtube.com/watch?v=AE-PS6-sL7U)
+
+    public partial class frmMainLogin : Form
+    {
+        public frmMainLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMainLogin_Load(object sender, EventArgs e)
+        {
+            // Connection string for Access 2007+:
+            //      @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\MyDatabase.accdb;Persist Security Info=False;";
+
+             // Connection string for older versions of Access:
+             //      @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\MyDatabase.mdb;User Id=admin;Password=;";
+            OleDbConnection connection = new OleDbConnection();
+            connection.ConnectionString =
+                @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=AccessLogin.mdb;User Id=admin;Password=;";
+            connection.Open();
+            if (connection.State == ConnectionState.Open)
+                lblStatus.Text = "Status = Open";
+            else
+                lblStatus.Text = "Status = ?";
+
+            connection.Close();
+        }
+    }
+}
