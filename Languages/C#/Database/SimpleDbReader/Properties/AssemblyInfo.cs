842e98a1e2af00911673339e76bd57accf2052e6
Alain Dekker <abdekker.online@gmail.com>
2021-02-24 15:39:54 +0000

    Common solution for all Visual Studio projects

--- Languages/C#/Database/SimpleDbReader/Properties/AssemblyInfo.cs
+++ Languages/C#/Database/SimpleDbReader/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("csSimpleDbReader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Database reader using DAO, ODBC and OleDB")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DekkerSoft")]
 [assembly: AssemblyProduct("SimpleDbReader")]
