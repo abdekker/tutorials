d565e04dec1d06fd53a5995815e37eae0864cb3d
Alain Dekker <abdekker.online@gmail.com>
2021-02-24 14:28:42 +0000

    Changing the name to show this is C# project

--- Languages/C#/DataTypes/Properties/AssemblyInfo.cs
+++ Languages/C#/DataTypes/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataTypes-CSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("csDataTypes")]
+[assembly: AssemblyDescription("Data types in C#")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataTypes-CSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany("DekkerSoft")]
+[assembly: AssemblyProduct("csDataTypes")]
+[assembly: AssemblyCopyright("Copyright © DekkerSoft 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
