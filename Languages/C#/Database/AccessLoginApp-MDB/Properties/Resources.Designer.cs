fcb7c4acb8f0c272916857d791b79d5c1665583a
Alain Dekker <abdekker.online@gmail.com>
2021-02-24 16:30:39 +0000

    Correcting reference to embedded resource after project rename

--- Languages/C#/Database/AccessLoginApp-MDB/Properties/Resources.Designer.cs
+++ Languages/C#/Database/AccessLoginApp-MDB/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace AccessLoginApp_MDB.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AccessLoginApp_MDB.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("csAccessLoginApp_MDB.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
