a6d9c7dc3819c69a7e41b773795d7899268d8f6b
Alain Dekker <abdekker.online@gmail.com>
2021-03-28 17:26:58 +0100

    Added comment to show how to test command-line arguments in Delphi

--- IDEs/Delphi7/SampleConsole/SampleConsole.dpr
+++ IDEs/Delphi7/SampleConsole/SampleConsole.dpr
@@ -11,28 +11,13 @@ begin
 	// Welcome message
 	WriteLn('Delphi 7 sample console application...');
 	WriteLn('');
+
+	// Show the command-line arguments. To test parameters inside the Delphi IDE:
+	// * Run > Parameters > Local
+	// * Type the required arguments in the "Parameters" box
 	ShowConsoleArguments();
 
-	// Show information about data types
-	//cout << "### Integers ###\n";
-	//cout << "\t\t\tSize (bytes)\tMin\t\t\tMax\n";
-	//{
-		// bool
-	  //	bool min = false;
-	   //	bool max = true;
-	   //	cout << "bool\t\t\t" << sizeof(bool) << "\t\t" << min << "\t\t\t" << max << "\n";
-		// Or: cout << formatter.formatString("bool\t\t\t%d\t\t%d\t\t\t%d\n", sizeof(bool), min, max);
-	//}
-
-	//{
-		// char, __int8
-	  //	char min = CHAR_MIN;
-	  //	char max = CHAR_MAX;
-	  //	unsigned char maxU = UCHAR_MAX;
-	  //  cout << formatter.formatString("char (__int8)\t\t%d\t\t%d\t\t\t%d\n", sizeof(char), min, max);
-	  //  cout << formatter.formatString("unsigned char\t\t%d\t\t0\t\t\t%u\n", sizeof(unsigned char), maxU);
-	//}
-
+	// Exit
 	WriteLn('Finished...press any key to exit');
 	while (not IsConsoleKeyPressed()) do;
 end.
